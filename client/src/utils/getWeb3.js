0d564879662b135b14916b2bfe19c3d60f545bc1
Travis Hairfield <travis.hairfield@gmail.com>
2018-10-07 12:59:31 -0700

    use ropsten infura as fallback

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -17,9 +17,8 @@ const getWeb3 = () =>
       } else {
         // Fallback to localhost if no web3 injection. We've configured this to
         // use the development console's port by default.
-        const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:9545"
-        );
+        const provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/f6NOUQqHkXc64NJgRwvj');
+        
         web3 = new Web3(provider);
         console.log("No web3 instance injected, using Local web3.");
         resolve(web3);
